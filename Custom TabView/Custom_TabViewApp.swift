58a76a746b2934b5be91f03e223e1977bc2493d8
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-06 01:50:54 +0200

    Initial Commit

---
+++ Custom TabView/Custom_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_TabViewApp.swift
+//  Custom TabView
+//
+//  Created by  Ixart on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
