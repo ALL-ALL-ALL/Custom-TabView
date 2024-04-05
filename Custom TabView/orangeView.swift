bc6af702988478049a5ccf0fabaaf62dee06694c
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-06 01:59:57 +0200

    en cours.......

---
+++ Custom TabView/orangeView.swift
@@ -0,0 +1,18 @@
+//
+//  orangeView.swift
+//  Custom TabView
+//
+//  Created by  Ixart on 06/04/2024.
+//
+
+import SwiftUI
+
+struct orangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    orangeView()
+}
---
+++ Custom TabView/vertView.swift
@@ -0,0 +1,18 @@
+//
+//  vertView.swift
+//  Custom TabView
+//
+//  Created by  Ixart on 06/04/2024.
+//
+
+import SwiftUI
+
+struct vertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    vertView()
+}
